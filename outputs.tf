e7eb5499f071c313f2cac21e76576b9845548d03
aravind kumar <aravindkumarreddy66@gmail.com>
2025-10-20 18:54:49 +0530

    terraform_vpc

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+/* output "az_info" { 
+    value = data.aws_availability_zones.available  
+} */
+
+/* output "default_vpc_info" {
+  value = data.aws_vpc.default
+} */
